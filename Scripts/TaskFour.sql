741dc12ee03e96a0fd7651cafef586258d9f77f3
dariatuz <84145244+dariatuz@users.noreply.github.com>
2025-05-26 19:10:22 +0000

    Min and Max sales

---
+++ Scripts/TaskFour.sql
@@ -0,0 +1,6 @@
+SELECT emp.employeeId, emp.firstName, emp.lastName, count(*) as NumOfCarsSold
+FROM sales sls
+INNER JOIN employee emp
+    ON sls.employeeId = emp.employeeId
+GROUP BY emp.employeeId, emp.firstName, emp.lastName
+ORDER BY NumOfCarsSold DESC;
